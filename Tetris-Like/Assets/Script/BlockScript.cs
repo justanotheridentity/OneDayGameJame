aea2cb10efb772d28e743e526d4ec2bf55d4b340
Timothe <t.lafranche@rubika-edu.com>
2019-01-16 10:34:42 +0100

    DeplacementBlock

---
+++ Tetris-Like/Assets/Script/BlockScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour {
+
+    public Vector2 position;
+    public int type = 0;
+    public bool isEnemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DestroyBlock()
+    {
+        // Avec un manager d'unité, accès à une liste des différentes unitées. Spawn en fonction du type actuel
+    }
+
+    public bool DetectionBlock()
+    {
+        bool blockDetected = false;
+        /* Appel du script de la Grille, pour récupérer le tableau
+         * Si position.x-/+ (En fonction de is enemy)1 != null
+         * Fin déplacement.
+         * */
+
+        return blockDetected;
+    }
+}
---
+++ Tetris-Like/Assets/Script/DeplacementFormScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementFormScript : MonoBehaviour {
+
+    public List<GameObject> blockChild;
+
+	// Use this for initialization
+	void Start () {
+        foreach(Transform child in transform)
+        {
+            blockChild.Add(child.gameObject);
+        }
+        StartCoroutine(DeplacementForme(1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DeplacementForme(float tempsDeplacement)
+    {
+
+        yield return new WaitForSeconds(tempsDeplacement);
+        foreach(GameObject child in blockChild)
+        {
+            if(child.GetComponent<BlockScript>().DetectionBlock())
+            {
+                foreach (GameObject block in blockChild)
+                {
+
+                }
+                yield return new WaitForSeconds(tempsDeplacement);
+            }
+        }
+        transform.position = new Vector2(Mathf.RoundToInt(transform.position.x + 1), Mathf.RoundToInt(transform.position.y));
+        foreach (GameObject child in blockChild)
+        {
+            child.GetComponent<BlockScript>().position.x = child.GetComponent<BlockScript>().position.x + 1;
+        }
+        StartCoroutine(DeplacementForme(tempsDeplacement));
+    }
+
+    void AjoutGrille()
+    {
+        /* Récupère la grille
+         * 
+         * 
+         * 
+         * */
+    }
+}

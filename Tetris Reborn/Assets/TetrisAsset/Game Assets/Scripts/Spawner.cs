46f6be0eea74eb74e8f925a7aa97fe618ddfedb9
Timothe <t.lafranche@rubika-edu.com>
2019-01-18 10:37:23 +0100

    LeTetrisMarche

---
+++ Tetris Reborn/Assets/TetrisAsset/Game Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] groups;
+    public int nextId;
+
+	// Use this for initialization
+	void Start () {
+        nextId = Random.Range(0, groups.Length);
+        spawnNext ();
+	}
+
+    public GameObject createGroup(Vector3 v) {
+        GameObject group = Instantiate(groups[nextId], v, Quaternion.identity);
+        return group;
+    }
+
+	// spawnNext group block
+	public void spawnNext() {
+		// Spawn Group at current Position
+        createGroup(transform.position);
+        //nextId = Random.Range(0, groups.Length);
+        nextId = 3;
+	}
+}
